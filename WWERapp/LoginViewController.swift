7800401b543cf942498073edafe886d3a99c7d92
Will Couture <wcouture17@gmail.com>
2021-11-08 20:08:56 -0500

    Add Login View Controller

---
+++ WWERapp/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  WWERapp
+//
+//  Created by Will Couture on 11/8/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
