c61fec4df30246d13ba9ec2ef06cea3b10866c12
Will Couture <wcouture17@gmail.com>
2021-11-08 20:16:21 -0500

    Update App Delegate

--- WWERapp/AppDelegate.swift
+++ WWERapp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "EtlirF7JhKRq7gF2PRvGmX6aNHvJzy7nafBSVvbt" // <- UPDATE
+                    $0.clientKey = "5LLrT0CLFHYLMfsrPqZLwVfZBI7nru1emYHBGQXT" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
