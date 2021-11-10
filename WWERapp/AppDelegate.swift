1bc8e569c5c2d765d4c34789524cf710955f38bd
ricardolasso <56412188+ricardolasso@users.noreply.github.com>
2021-11-09 20:10:54 -0500

    Database
    Database

--- WWERapp/AppDelegate.swift
+++ WWERapp/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let parseConfig = ParseClientConfiguration {
-                    $0.applicationId = "00i9cPYqcUrJblYOnFieg8wkMWyNxyUWcd3rwuYZ" // <- UPDATE
-                    $0.clientKey = "xzihQlhwxBIaoPSOPWQN1dvsjbGxI5mwTFtPTiUd" // <- UPDATE
+                    $0.applicationId = "CjTDNLO6dBj7c0ijYJ6veTEM503UJCMkXcErWGtC" // <- UPDATE
+                    $0.clientKey = "XkBq8SimWAsA4PvEYty4rjhPyneDz9CJoyqFig88" // <- UPDATE
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
