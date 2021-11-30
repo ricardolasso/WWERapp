df981c645bba86bb8c2a1dc8a5502e62b9cce521
Writer Leto <edgebagwhat@gmail.com>
2021-11-30 15:07:13 -0500

    added description menu need to add descriptions

---
+++ WWERapp/DetailsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailsViewController.swift
+//  WWERapp
+//
+//  Created by user203102 on 11/30/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+            
+    
+   
+    @IBAction func onPlayStationButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onXboxOneXButton(_ sender: Any) {
+    
+    }
+    @IBAction func onXboxOneSButton(_ sender: Any) {
+    
+    }
+    @IBAction func onNintendoButton(_ sender: Any) {
+    
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
